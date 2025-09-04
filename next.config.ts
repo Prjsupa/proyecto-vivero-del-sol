2593d0176eda15d5e17f343f5cafb06015efc1ad
prj <prjsupa@gmail.com>
2025-09-04 02:25:50 +0000

    Error: Invalid src prop (https://fqkxbtahfsiebrphgzwg.supabase.co/storag

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'fqkxbtahfsiebrphgzwg.supabase.co',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
